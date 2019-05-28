74f6dd9583a116e9c332dcbb7c94275a0e2615fb
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2019-05-28 13:00:52 +0200

    Added cpu check and some refactor

--- benchmark.py
+++ benchmark.py
@@ -7,11 +7,28 @@ import socket
 import subprocess
 import sys
 import time
-
+import argparse
 
 fqdn = socket.getfqdn()
 num_tests = 10
-max_time = '300'
+data = {}
+
+# Parser
+actions = ['memory', 'cpu']
+parser = argparse.ArgumentParser()
+subparser = parser.add_subparsers(title='action')
+
+for action in actions:
+    a = subparser.add_parser(action)
+    a.set_defaults(action=action)
+    a.add_argument('time', metavar='time')
+
+options = parser.parse_args()
+
+global_args = [
+    '--threads=' + str(multiprocessing.cpu_count()),
+    '--time=' + options.time
+]
 
 def parse_output(output, data):
     """ Parse sysbench text output and store the metric in a dict """
@@ -36,19 +53,24 @@ def run_test(name, global_args, **kwargs):
     #print command
     return subprocess.check_output(command)
 
-def main(args):
-    args.pop(0)
-    if len(args) == 0:
-        print >>sys.stderr, "Need at max time input"
-        return 1
+def action_io():
+    pass
 
-    global_args = [
-        '--threads=' + str(multiprocessing.cpu_count()),
-        '--time=' + args.pop()
-    ]
-    data = {}
+def action_cpu():
+    max_prime = ['1000', '5000', '10000', '20000', '30000', '50000', '100000', '200000', '500000', '1000000' ]
+    name = 'cpu'
+    data[name] = {}
+    for i in range(num_tests):
+        kwargs = {'cpu_max_prime': max_prime[i]}
+        data[name][i] = {}
+        output = run_test(name, global_args, **kwargs)
+        time.sleep(1)
+        #print output
+        parse_output(output, data[name][i])
+        data[name][i]['hostname'] = fqdn
+    write_file(name + '.js', data)
 
-    # Memory
+def action_memory():
     memory_blocks = ['1K', '4K','16K', '64K', '128K', '1M', '4M', '8M', '16M', '64M']
     kwargs = {'memory_total_size': '100T', 'memory_access_mode': 'rnd'}
     name = 'memory'
@@ -60,11 +82,28 @@ def main(args):
         time.sleep(1)
         #print output
         parse_output(output, data[name][i])
+        data[name][i]['hostname'] = fqdn
+    write_file(name + '.js', data)
 
+def write_file(file, data):
     # Output json
-    with open('benchmark.js', 'w') as outfile:
+    with open(file, 'w') as outfile:
         json.dump(data, outfile, indent=2)
 
+def main(args):
+    args.pop(0)
+    if len(args) == 0:
+        print >>sys.stderr, "Need at max time input"
+        return 1
 
-if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+
+
+
+
+
+# Run local function with the same name as the action (Note: - => _)
+action = locals().get('action_' + options.action.replace('-', '_'))
+if not action:
+    print("Function action_%s not implemented" % options.action)
+    sys.exit(1)
+action()
